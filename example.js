4b6d74f5e041fcd1f568716fe80c29944e104839
helio-frota <00hf11@gmail.com>
2016-08-24 16:21:49 -0300

    This closes #3

--- example.js
+++ example.js
@@ -5,19 +5,19 @@ const registrationClient = require('./');
 const baseUrl = 'http://localhost:8082/ag-push';
 
 registrationClient(baseUrl).then((client) => {
-    const settings = {
-        variantId: 'c82e79c0-1c26-45dc-b482-8f75ce35510d',
-        secret: '9a2906ee-95da-45d6-8fdb-643b2d0f9a43'
-    };
+  const settings = {
+    variantId: 'c82e79c0-1c26-45dc-b482-8f75ce35510d',
+    secret: '9a2906ee-95da-45d6-8fdb-643b2d0f9a43'
+  };
 
-    const deviceOptions ={
-        deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
-        alias: 'android node'
-    };
+  const deviceOptions = {
+    deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
+    alias: 'android node'
+  };
 
-    return client.registry.registerDevice(settings, deviceOptions).then((deviceRegistration) => {
-        console.log(deviceRegistration);
-    });
+  return client.registry.registerDevice(settings, deviceOptions).then((deviceRegistration) => {
+    console.log(deviceRegistration);
+  });
 }).catch((err) => {
-    console.log('error', err);
+  console.log('error', err);
 });
--- test/integration/registry-test.js
+++ test/integration/registry-test.js
@@ -1,247 +1,250 @@
 'use strict';
 
 const test = require('tape');
+const path = require('path');
 const fs = require('fs');
 const registrationClient = require('../../');
 const adminClient = require('unifiedpush-admin-client');
 
-const importerFileLocation =  __dirname + '/../../build/importer-test.json';
+const importerFileLocation = path.join(__dirname, '/../../build/importer-test.json');
 
 const baseUrl = 'http://localhost:8082/ag-push';
 
-
-// First need to setup a Push Application and Variant(we will use Android for the test);
+// First need to setup a Push Application and Variant(we will use Android for the test)
 const setup = () => {
-    const admingSettings = {
-        username: 'admin',
-        password: 'admin',
-        kcUrl: 'http://localhost:8080/auth',
-        kcRealmName: 'master'
-    };
+  const admingSettings = {
+    username: 'admin',
+    password: 'admin',
+    kcUrl: 'http://localhost:8080/auth',
+    kcRealmName: 'master'
+  };
 
-    return adminClient(baseUrl, admingSettings)
-        .then((client) => {
-            return client.applications.create({name: 'For Android Variant'}).then((application) => {
-                const variantOptions = {
-                    pushAppId: application.pushApplicationID,
-                    name: 'Android Variant',
-                    type: 'android',
-                    android: {
-                        googleKey: 'abcd-1234',
-                        'projectNumber': '1234567'
-                    }
-                };
+  return adminClient(baseUrl, admingSettings)
+    .then((client) => {
+      return client.applications.create({name: 'For Android Variant'}).then((application) => {
+        const variantOptions = {
+          pushAppId: application.pushApplicationID,
+          name: 'Android Variant',
+          type: 'android',
+          android: {
+            googleKey: 'abcd-1234',
+            'projectNumber': '1234567'
+          }
+        };
 
-                return client.variants.create(variantOptions).then((variant) => {
-                    variant.pushAppId = application.pushApplicationID;
-                    return variant;
-                });
-            });
+        return client.variants.create(variantOptions).then((variant) => {
+          variant.pushAppId = application.pushApplicationID;
+          return variant;
         });
+      });
+    });
 };
 
 const remove = (pushAppId) => {
-    const admingSettings = {
-        username: 'admin',
-        password: 'admin',
-        kcUrl: 'http://localhost:8080/auth',
-        kcRealmName: 'master'
-    };
+  const admingSettings = {
+    username: 'admin',
+    password: 'admin',
+    kcUrl: 'http://localhost:8080/auth',
+    kcRealmName: 'master'
+  };
 
-    return adminClient(baseUrl, admingSettings).then((client) => {
-        return client.applications.remove(pushAppId);
-    });
+  return adminClient(baseUrl, admingSettings).then((client) => {
+    return client.applications.remove(pushAppId);
+  });
 };
 
-
 test('Register Device - sucess', (t) => {
-    const deviceOptions ={
-        deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
-        alias: 'Android Thing'
-    };
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret
-            };
+  const deviceOptions = {
+    deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
+    alias: 'Android Thing'
+  };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret
+      };
 
-            t.equals(typeof client.registry.registerDevice, 'function', 'should have the registerDevice function');
+      t.equals(typeof client.registry.registerDevice, 'function', 'should have the registerDevice function');
 
-            return client.registry.registerDevice(settings, deviceOptions).then((device) => {
-                t.equal(device.deviceToken, deviceOptions.deviceToken, 'deviceTokens should be the same');
-                t.equal(device.alias, deviceOptions.alias, 'alias should be the same');
+      return client.registry.registerDevice(settings, deviceOptions).then((device) => {
+        t.equal(device.deviceToken, deviceOptions.deviceToken, 'deviceTokens should be the same');
+        t.equal(device.alias, deviceOptions.alias, 'alias should be the same');
 
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
 
 test('Register Device - fail on missing deviceToken', (t) => {
-    const deviceOptions ={
-        alias: 'Android Thing'
-    };
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret
-            };
+  const deviceOptions = {
+    alias: 'Android Thing'
+  };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret
+      };
 
-            t.equals(typeof client.registry.registerDevice, 'function', 'should have the registerDevice function');
+      t.equals(typeof client.registry.registerDevice, 'function', 'should have the registerDevice function');
 
-            return client.registry.registerDevice(settings, deviceOptions).catch((err) => {
-                t.equal(err, 'Bad Request', 'should be a bad request error');
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+      return client.registry.registerDevice(settings, deviceOptions).catch((err) => {
+        t.equal(err, 'Bad Request', 'should be a bad request error');
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
 
 test('unRegister Device - sucess', (t) => {
-    const deviceOptions ={
-        deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
-        alias: 'Android Thing'
-    };
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret
-            };
+  const deviceOptions = {
+    deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
+    alias: 'Android Thing'
+  };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret
+      };
 
-            t.equals(typeof client.registry.unregisterDevice, 'function', 'should have the unregisterDevice function');
+      t.equals(typeof client.registry.unregisterDevice, 'function', 'should have the unregisterDevice function');
 
-            // Just need to register a device first
-            return client.registry.registerDevice(settings, deviceOptions).then((device) => {
-                return device;
-            }).then((device) => {
-                settings.deviceToken = device.deviceToken;
-                return client.registry.unregisterDevice(settings);
-            }).then(() => {
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+      // Just need to register a device first
+      return client.registry.registerDevice(settings, deviceOptions).then((device) => {
+        return device;
+      }).then((device) => {
+        settings.deviceToken = device.deviceToken;
+        return client.registry.unregisterDevice(settings);
+      }).then(() => {
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
 
 test('unRegister Device - fail', (t) => {
-    const deviceOptions ={
-        deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
-        alias: 'Android Thing'
-    };
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret
-            };
+  const deviceOptions = {
+    deviceToken: 'c8apwu2iJvc:APA91bGRBqDHCl15jqyjDVyHk8irqz8pyiLR0QHYSqRdNF844SlmlrwRZ2vuek56_lcfBeXCxeExP51reF7ESVnygE6Y6qpxrkBur2d5-aC6QcfaRFuPP6qbQmO93e4YhpmD3HalExL6',
+    alias: 'Android Thing'
+  };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret
+      };
 
-            t.equals(typeof client.registry.unregisterDevice, 'function', 'should have the unregisterDevice function');
+      t.equals(typeof client.registry.unregisterDevice, 'function', 'should have the unregisterDevice function');
 
-            // Just need to register a device first
-            return client.registry.registerDevice(settings, deviceOptions).then((device) => {
-                return device;
-            }).then((device) => {
-                settings.deviceToken = 'WRONG_TOKEN';
-                return client.registry.unregisterDevice(settings);
-            }).catch((err) => {
-                t.pass('should fail');
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+      // Just need to register a device first
+      return client.registry.registerDevice(settings, deviceOptions).then((device) => {
+        return device;
+      }).then((device) => {
+        settings.deviceToken = 'WRONG_TOKEN';
+        return client.registry.unregisterDevice(settings);
+      }).catch((error) => {
+        if (error) {
+          console.error(error);
+        }
+        t.pass('should fail');
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
 
 test('test importer - file as a string- success', (t) => {
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret
-            };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret
+      };
 
-            t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
+      t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
 
-            // Just need to register a device first
-            return client.registry.importer(settings, importerFileLocation).then(() => {
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+      // Just need to register a device first
+      return client.registry.importer(settings, importerFileLocation).then(() => {
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
 
 test('test importer - file as a object- success', (t) => {
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret
-            };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret
+      };
 
-            t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
+      t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
 
-
-            const devices = require(importerFileLocation);
-            // Just need to register a device first
-            return client.registry.importer(settings, devices).then(() => {
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+      const devices = require(importerFileLocation);
+      // Just need to register a device first
+      return client.registry.importer(settings, devices).then(() => {
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
 
-
 test('test importer - file as a readStream- success', (t) => {
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret
-            };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret
+      };
 
-            t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
+      t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
 
-            // Just need to register a device first
-            return client.registry.importer(settings, fs.createReadStream(importerFileLocation)).then(() => {
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+      // Just need to register a device first
+      return client.registry.importer(settings, fs.createReadStream(importerFileLocation)).then(() => {
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
 
 test('test importer - file as a string- failure', (t) => {
-    // Once we have all the setup,  Register
-    setup().then((variant) => {
-        return registrationClient(baseUrl).then((client) => {
-            const settings = {
-                variantId: variant.variantID,
-                secret: variant.secret + '1'
-            };
+  // Once we have all the setup,  Register
+  setup().then((variant) => {
+    return registrationClient(baseUrl).then((client) => {
+      const settings = {
+        variantId: variant.variantID,
+        secret: variant.secret + '1'
+      };
 
-            t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
+      t.equals(typeof client.registry.importer, 'function', 'should have the importer function');
 
-            // Just need to register a device first
-            return client.registry.importer(settings, importerFileLocation).catch((err) => {
-                t.pass('should fail');
-                remove(variant.pushAppId);
-                t.end();
-            });
-        });
+      // Just need to register a device first
+      return client.registry.importer(settings, importerFileLocation).catch((error) => {
+        if (error) {
+          console.error(error);
+        }
+        t.pass('should fail');
+        remove(variant.pushAppId);
+        t.end();
+      });
     });
+  });
 });
